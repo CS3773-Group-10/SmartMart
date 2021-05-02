07efdd9055c329fb737bec4ed8219cb1dcb433ee
Raquel <raquelchu2010@gmail.com>
2021-05-02 17:04:59 -0500

    login layout

--- src/application/Controller/LoginController.java
+++ src/application/Controller/LoginController.java
@@ -1,4 +1,16 @@
 package application.Controller;
 
+
+import javafx.fxml.FXML;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
 public class LoginController {
+
+    @FXML
+    private Button cancelButton;
+
 }
