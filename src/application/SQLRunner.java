6a8f84b1ceec4d1705f8c7c2884db6ee7a11559d
Vanessa <hpj766@my.utsa.edu>
2021-05-02 16:53:28 -0500

    Initalize Customers Table

---
+++ src/application/SQLRunner.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.Scanner;
+
+public class SQLRunner {
+
+    private static final String DELIMITER = ";";
+
+    public void runScript(Connection connection, File file)
+            throws FileNotFoundException, SQLException {
+        Scanner scanner = new Scanner(file).useDelimiter(DELIMITER);
+        Statement statement = connection.createStatement();
+
+        while (scanner.hasNext()) {
+            String nextStatement = scanner.next();
+            System.out.println(nextStatement);
+            if (!nextStatement.trim().isEmpty()) {
+                statement.execute(nextStatement);
+            }
+        }
+        statement.close();
+        scanner.close();
+    }
+
+}
