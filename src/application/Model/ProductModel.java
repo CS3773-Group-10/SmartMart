8e433c435423f84562e50832b1c16781efd4df86
Vanessa <hpj766@my.utsa.edu>
2021-05-03 04:36:49 -0500

    Product Model Planned

---
+++ src/application/Model/ProductModel.java
@@ -0,0 +1,34 @@
+package application.Model;
+
+public class ProductModel {
+
+
+    /**
+     * getInventory()
+     * Gets the entire inventory of products as an array of all product IDs
+     *
+     * @return  list of product IDs for all products in the database
+     */
+    public int[] getInventory() {
+        // TODO: get product list
+
+        return new int[0];
+    }
+
+    // TODO: set product quantity
+
+    // TODO: get list of categories
+
+    // TODO: get product name by id
+
+    // TODO: get product description by id
+
+    // TODO: get product category
+
+    // TODO: get product quantity in stock
+
+    // TODO: get product sell by
+
+    // TODO: get product image -- we should add a folder of product images with the file name "product-"+ id +".png"
+
+}
