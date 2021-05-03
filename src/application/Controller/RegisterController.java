530499c03001076c456e9a80823d2a9f9caa8cb2
Raquel <raquelchu2010@gmail.com>
2021-05-03 03:25:54 -0500

    Register Layout

--- src/application/Controller/RegisterController.java
+++ src/application/Controller/RegisterController.java
@@ -1,4 +1,50 @@
 package application.Controller;
 
-public class RegisterController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class RegisterController implements Initializable {
+
+    @FXML
+    private ImageView registerImage;
+
+    @FXML
+    private TextField firstNameTextField;
+
+    @FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private PasswordField comfirmPasswordField;
+
+    @FXML
+    private Button registerButton;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        File registerFile = new File( "images/register.png");
+        Image rImage = new Image(registerFile.toURI().toString());
+        registerImage.setImage(rImage);
+
+    }
 }
