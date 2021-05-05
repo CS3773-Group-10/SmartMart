7d0aaa9e583d701649cf9adf5dd5643ee38617ab
coreyRam <carlysdell@yahoo.com>
2021-05-05 11:34:52 -0500

    RegisterController will go to CategoryList now

--- src/application/Controller/RegisterController.java
+++ src/application/Controller/RegisterController.java
@@ -70,15 +70,40 @@ public class RegisterController implements Initializable {
         String comfirmPassword = comfirmPasswordField.getText();
 
         // if passwords match, add customer using Customer Model
-
-        // TODO: verify password and confirm password is same
         CustomerModel cm = new CustomerModel();
         if(password.equals(comfirmPassword)){
-            cm.addCustomer(firstName, lastName, email, password);
+            int userId = cm.addCustomer(firstName, lastName, email, password);
+            
+            try {
+                goToCategoryList(event, userId);
+            }
+            catch (IOException e) {
+                //something
+            }
         } else{
             registerMessageLabel.setText("Password doesn't match. Please, try again.");
         }
 
+    }
+
+    @FXML
+    private void goToCategoryList(ActionEvent event, int id) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+
+        //content root is set to src, this may not work if content root set to something else
+        loader.setLocation(getClass().getResource("/application/View/categoryList.fxml"));
+        loader.load();
+
+        //get the controller that the fxml is linked to and update the userId
+        CategoryListController controller = loader.getController();
+        controller.setUserId(id);
+
+        AnchorPane p = (AnchorPane) loader.getRoot();
+        Scene scene = new Scene(p, 360, 640);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.setResizable(false);
+        window.show();
     }
 
     public void goBack(ActionEvent event) throws IOException {
