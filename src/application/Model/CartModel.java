f2f00983c4d9ed795fa207e3c566fde51986eaa6
Brynna <68093791+bwatring@users.noreply.github.com>
2021-05-06 18:54:30 -0500

    Update CartModel.java

--- src/application/Model/CartModel.java
+++ src/application/Model/CartModel.java
@@ -1,9 +1,13 @@
 package application.Model;
 
+import application.Main;
+
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CartModel {
+    private static Connection conn = Main.conn;
 
     /**
      * addToCart(custId, productId, quantity)
@@ -21,7 +25,7 @@ public class CartModel {
                 "VALUES (?, ?, ?)");
         preparedStatement.setInt(1, custId);
         preparedStatement.setInt(2, productId);
-        preparedStatement.setInt((3, quantity);
+        preparedStatement.setInt(3, quantity);
         preparedStatement.executeUpdate();
     }
 
@@ -30,28 +34,21 @@ public class CartModel {
      * removeFromCart(cartId)
      * removes the cart item from the database
      *
-     * @param cartId - the cart that the customer item is associated with
+     * @param cartId
      * @throws SQLException
      */
     public void removeFromCart(int cartId) throws SQLException {
-        PreparedStatement preparedStatement = conn.prepareStatement(
-            "Delete cart from data base:"
-            printf("Deleted cart:", cartId);
-            preparedStatement.executeUpdate();
-        
         // TODO: removeFromCart(cartId) - removes the cart item from the database
-        // use executeUpdate to perform a DELETE   
-        
-     
-        
+        // use executeUpdate to perform a DELETE
     }
+
 
     /**
      * clearCart(custId)
      * clears the customers cart.
      *
-     * @param custId - customer the cart item is associated with
-     * @throws SQLException 
+     * @param custId
+     * @throws SQLException
      */
     public void clearCart(int custId) throws SQLException {
         // TODO: clearCart(custId) - clears the customers cart. removes all cart items associated with the custId. (This will be utilized when a customer converts their cart into an order)
@@ -60,64 +57,10 @@ public class CartModel {
     // TODO: getCart(custId) - returns list of cart ids associated with the custID
     // use executeQuery to SELECT data
 
-   
-                                 
-     /**
-     * getProduct(cartId)
-     * returns productId associated with the cartId
-     *
-     * @param cartId - the cart item the customer is associated with 
-     * @throws SQLException
-     */                             
-    public void getProduct( int cartId, int productId ) throws SQLException {
-       PreparedStatement preparedStatement = conn.prepareStatement(
-           // search through cartId and find match
-           //if match return product 
-            "Inset Cart ID cartId"+
-                "(custID, productID)"+
-                "VALUES (?, ?)");
-        preparedStatement.executeUpdate();
-    }
-        
-        
-        
-        
+    // TODO: getProduct(cartId) - returns product id associated with the cartId
 
-    
-     /**
-     * getQuantity(cartId)
-     * returns quantity added to cart at cartId
-     *
-     * @param cartId - the cart number the customer is associated with 
-     * @throws SQLException
-     */  
-    public void getQuantity( int cartId) throws SQLException {
-    
-        
-    }
-            
-    
-  
-                                 
+    //TODO: getQuantity(cartId) - returns quantity added to cart at cartId
 
-        
-        
-     /**
-     * setQuantity(cartId)
-     * sets the quantity added to cart for cart item at cartId
-     *
-     * @param cartId - the cart number the customer is associated with 
-     * @throws SQLException
-     */
-      public void setQuantity(int cartId) throws SQLException {
-     
-      // set quantity 
-      // 
-          
-          
-          
-          
-      }
+    // TODO: setQuantity(cartId) - sets the quantity added to cart for cart item at cartId
 
-
-
+}
